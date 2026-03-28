eb1f555461eaa286b7992d121b6c4d7bb2038aa9
tmblack01 <thomasbl5656@gmail.com>
2026-03-28 14:02:44 +1100

    Created setup script and updated renv.lock file

---
+++ scripts/00_setup.R
@@ -0,0 +1,7 @@
+## 0: Setup
+## Script for setting up packages and dependencies for the analysis
+
+# --- Packages -----------------------------------------------------------------
+
+library(tidyverse)
+library(GEOquery)
